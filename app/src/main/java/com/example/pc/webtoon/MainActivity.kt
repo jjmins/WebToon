a49ba71548ecc753787622ddfe40bf12948c7685
gotsejeong <dbdnpxm@naver.com>
2018-01-22 17:44:33 +0900

    adf

---
+++ app/src/main/java/com/example/pc/webtoon/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.pc.webtoon
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(mainToolbar)
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return true
+    }
+}
